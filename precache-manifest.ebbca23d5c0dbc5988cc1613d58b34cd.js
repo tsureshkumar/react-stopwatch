1546d2c8f8f3d7d24143f41a130b20bbe2f4700e
Sureshkumar T <sureshkumar.thangavel@walmartlabs.com>
2020-04-19 02:04:53 +0530

    initial release

---
+++ precache-manifest.ebbca23d5c0dbc5988cc1613d58b34cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f3270c496ef4ce44fbc8ec37696d40f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "062f0bc05d013a1618ce",
+    "url": "/static/css/main.ebcbc7d2.chunk.css"
+  },
+  {
+    "revision": "5b32e2fdaa9e7d6af3eb",
+    "url": "/static/js/2.0dba5d72.chunk.js"
+  },
+  {
+    "revision": "775071e7908c30b629946039840806b0",
+    "url": "/static/js/2.0dba5d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062f0bc05d013a1618ce",
+    "url": "/static/js/main.504a1fda.chunk.js"
+  },
+  {
+    "revision": "cba7f8183bf7f143e9a7",
+    "url": "/static/js/runtime-main.ddbad2c1.js"
+  },
+  {
+    "revision": "a19cddcf2230a1cf6fa3210ec9b4df85",
+    "url": "/static/media/Archivo-Bold.a19cddcf.ttf"
+  },
+  {
+    "revision": "4b36aaa8578d7858fd166b60b5b3c885",
+    "url": "/static/media/Archivo-Regular.4b36aaa8.ttf"
+  }
+]);
