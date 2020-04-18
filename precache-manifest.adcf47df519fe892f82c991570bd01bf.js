e0aafb643b3151c370d906cca3e77b14ec376859
Sureshkumar T <sureshkumar.thangavel@walmartlabs.com>
2020-04-19 02:19:53 +0530

    publishing to gh-pages

---
+++ precache-manifest.adcf47df519fe892f82c991570bd01bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dafb5f57aa0dac1c8e2ed7c102383716",
+    "url": "https://tsureshkumar.github.io/react-stopwatch/index.html"
+  },
+  {
+    "revision": "e57c1c7211693e17e934",
+    "url": "https://tsureshkumar.github.io/react-stopwatch/static/css/main.1bd1dae1.chunk.css"
+  },
+  {
+    "revision": "5b32e2fdaa9e7d6af3eb",
+    "url": "https://tsureshkumar.github.io/react-stopwatch/static/js/2.0dba5d72.chunk.js"
+  },
+  {
+    "revision": "775071e7908c30b629946039840806b0",
+    "url": "https://tsureshkumar.github.io/react-stopwatch/static/js/2.0dba5d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e57c1c7211693e17e934",
+    "url": "https://tsureshkumar.github.io/react-stopwatch/static/js/main.80624836.chunk.js"
+  },
+  {
+    "revision": "41d45510afcfab471c8e",
+    "url": "https://tsureshkumar.github.io/react-stopwatch/static/js/runtime-main.4a7efc2b.js"
+  },
+  {
+    "revision": "a19cddcf2230a1cf6fa3210ec9b4df85",
+    "url": "https://tsureshkumar.github.io/react-stopwatch/static/media/Archivo-Bold.a19cddcf.ttf"
+  },
+  {
+    "revision": "4b36aaa8578d7858fd166b60b5b3c885",
+    "url": "https://tsureshkumar.github.io/react-stopwatch/static/media/Archivo-Regular.4b36aaa8.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ebbca23d5c0dbc5988cc1613d58b34cd.js"
+  "https://tsureshkumar.github.io/react-stopwatch/precache-manifest.adcf47df519fe892f82c991570bd01bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://tsureshkumar.github.io/react-stopwatch/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
